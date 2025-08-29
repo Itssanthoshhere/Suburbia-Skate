7f17adc82f26e69e3fb229fc73d8c0bde13d2eed
Santhosh VS <santhosh02vs@gmail.com>
2025-08-29 22:23:35 +0530

    feat(scene): add lighting and floor to skateboard preview

--- src/app/build/Preview.tsx
+++ src/app/build/Preview.tsx
@@ -27,6 +27,7 @@ type Props = {
 
 export default function Preview({ wheelTextureURLs, deckTextureURLs }: Props) {
     const cameraControls = useRef<CameraControls>(null);
+    const floorRef = useRef<THREE.Mesh>(null);
 
     const { selectedWheel, selectedBolt, selectedDeck, selectedTruck } =
         useCustomizerControls();
@@ -39,14 +40,39 @@ export default function Preview({ wheelTextureURLs, deckTextureURLs }: Props) {
     const boltColor = selectedBolt?.color ?? DEFAULT_BOLT_COLOR;
 
 
+    function onCameraControlStart() {
+        if (
+            !cameraControls.current ||
+            !floorRef.current ||
+            cameraControls.current.colliderMeshes.length > 0
+        )
+            return;
+
+        cameraControls.current.colliderMeshes = [floorRef.current];
+    }
+
     return (
-        <Canvas>
+        <Canvas camera={{ position: [2.5, 1, 0], fov: 50 }} shadows>
             <Suspense fallback={null}>
                 <Environment
                     files={"/hdr/warehouse-512.hdr"}
                     environmentIntensity={0.6}
                 />
-            
+                <directionalLight
+                    castShadow
+                    lookAt={[0, 0, 0]}
+                    position={[1, 1, 1]}
+                    intensity={1.6}
+                />
+                <fog attach="fog" args={[ENVIRONMENT_COLOR, 3, 10]} />
+                <color attach="background" args={[ENVIRONMENT_COLOR]} />
+                <StageFloor />
+
+                <mesh rotation={[-Math.PI / 2, 0, 0]} ref={floorRef}>
+                    <planeGeometry args={[6, 6]} />
+                    <meshBasicMaterial visible={false} />
+                </mesh>
+
                 <Skateboard
                     wheelTextureURLs={wheelTextureURLs}
                     wheelTextureURL={wheelTexureURL}
@@ -56,8 +82,12 @@ export default function Preview({ wheelTextureURLs, deckTextureURLs }: Props) {
                     boltColor={boltColor}
                     pose="side"
                 />
-
-              
+                <CameraControls
+                    ref={cameraControls}
+                    minDistance={0.2}
+                    maxDistance={4}
+                    onStart={onCameraControlStart}
+                />
             </Suspense>
             <Preload all />
         </Canvas>
@@ -65,3 +95,28 @@ export default function Preview({ wheelTextureURLs, deckTextureURLs }: Props) {
 }
 
 
+function StageFloor() {
+    const normalMap = useTexture("/concrete-normal.avif");
+    normalMap.wrapS = THREE.RepeatWrapping;
+    normalMap.wrapT = THREE.RepeatWrapping;
+    normalMap.repeat.set(30, 30);
+    normalMap.anisotropy = 8;
+
+    const material = new THREE.MeshStandardMaterial({
+        roughness: 0.75,
+        color: ENVIRONMENT_COLOR,
+        normalMap: normalMap,
+    });
+
+    return (
+        <mesh
+            castShadow
+            receiveShadow
+            position={[0, -0.005, 0]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            material={material}
+        >
+            <circleGeometry args={[20, 32]} />
+        </mesh>
+    );
+}
